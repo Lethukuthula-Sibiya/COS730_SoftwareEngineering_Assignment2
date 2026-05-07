d38f9f7853797cb8e1a1b491c648d0c1d7d28c8e
LM SIBIYA <116903062+Lethukuthula-Sibiya@users.noreply.github.com>
2026-05-07 19:18:44 +0200

    Delete Task1_Original directory

--- Task1_Original/Validator.cs
+++
@@ -1,8 +0,0 @@
-public class Validator
-{
-    public bool ValidateFormat(string data)
-    {
-        Console.WriteLine("Validator: Validating format...");
-        return !string.IsNullOrWhiteSpace(data);
-    }
-}
